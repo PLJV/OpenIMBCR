0297d01235134ad8cec90e76a4eeb384e074e7ce
Kyle Taylor <ktaylora@users.noreply.github.com>
2017-10-30 12:35:14 -0600

    Update selection.R

--- R/selection.R
+++ R/selection.R
@@ -1,3 +1,20 @@
+# Author: Kyle Taylor <kyle.taylor@pljv.org>
+# Year : 2016
+# Description : various tasks related to model selection and optimization
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #' downsample records in a continuous 'distances' distribution using a
 #' quantile cut-off. Distances can be literal, or the output of a Mahalanobis 
 #' or Euclidean distance function (see: the 'FNN'package). This function is 
