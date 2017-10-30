e8dff5c22dfe684b22b1afd04c74d00d1074a874
Kyle Taylor <ktaylora@users.noreply.github.com>
2017-10-30 12:34:40 -0600

    Update reporting.R

--- R/reporting.R
+++ R/reporting.R
@@ -1,3 +1,21 @@
+# Author: Kyle Taylor <kyle.taylor@pljv.org>
+# Year : 2016
+# Description : various tasks related to making variable response plots 
+# and visualizations from unmarked and other objects.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #' build a response plot for a given variable using a fitted unmarked model
 #' @export
 partialPredict <- function(m=NULL, var=NULL, type='state', plot=T, nCores=NULL,
