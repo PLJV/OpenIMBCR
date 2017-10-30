4e0ef91d459c4bcbfac1ef052b4813a817cada20
Kyle Taylor <ktaylora@users.noreply.github.com>
2017-10-30 12:31:58 -0600

    Update kludging.R

--- R/kludging.R
+++ R/kludging.R
@@ -1,3 +1,21 @@
+# Author: Kyle Taylor <kyle.taylor@pljv.org>
+# Year : 2017
+# Description : various tasks related to formatting and parsing raw IMBCR data
+# into Spatial* and Unmarked* objects.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #' built-in (hidden) function that will accept the full path of a shapefile and parse the string into something that
 #' rgdal can understand (DSN + Layer).
 parseLayerDsn <- function(x=NULL){
